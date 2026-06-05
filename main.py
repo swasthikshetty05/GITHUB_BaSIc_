6f2ceeb197956d842790bba7bbc700ca3160340b
Swasthik <swasthikshetty001@gmail.com>
2026-06-05 12:35:23 +0530

    anything

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/abc")
+def abc():
+    return render_template("abc.html")
+
+
+@app.route("/<name>")
+def greet(name):
+    return render_template("greet.html", name=name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    

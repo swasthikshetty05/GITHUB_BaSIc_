6a9a754046de9596f9235387efaaac78da03f26c
Swasthik <swasthikshetty001@gmail.com>
2026-06-05 14:15:37 +0530

    anything

--- main.py
+++ main.py
@@ -6,17 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-
 @app.route("/abc")
 def abc():
     return render_template("abc.html")
 
-
-@app.route("/<name>")
+@app.route("/greet/<name>")
 def greet(name):
     return render_template("greet.html", name=name)
 
-
 if __name__ == "__main__":
     app.run(debug=True)
-    
